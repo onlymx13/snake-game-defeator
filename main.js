2bccec3e256c513799b367f67cc32e675d90bc97
onlymx13 <onlymx13@gmail.com>
2018-04-27 17:06:35 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var x;
 var y;
 var dir;
-Array.prototype.random = function () {
-  return this[Math.floor((Math.random()*this.length))];
+Array.prototype.random = function() {
+    return this[Math.floor((Math.random() * this.length))];
 }
+
 function main() {
     img = document.getElementById('myImage');
     file = document.querySelector('input[type=file]').files[0];
@@ -20,26 +21,31 @@ function main() {
     var context = mycanvas.getContext('2d');
     context.drawImage(img, 0, 0, img.width, img.height);
     var array = [];
-    for (x = 1; x <= mycanvas.width;x++) {
-        for (y = 1; y<= mycanvas.height; y++) {
-            var data = context.getImageData(x,y,1,1).data;
-            if ([data[0],data[1],data[2]] == [150,150,150]) { array.push([x, y]); }
+    for (x = 1; x <= mycanvas.width; x++) {
+        for (y = 1; y <= mycanvas.height; y++) {
+            var data = context.getImageData(x, y, 1, 1).data;
+            if ([data[0], data[1], data[2]] == [150, 150, 150]) {
+                array.push([x, y]);
+            }
         }
     }
-    var bluePixel = context.createImageData(1,1);
-    bluePixel.data = [0,113,255,255];
-    while ([context.getImageData(x,y,1,1).data[0],context.getImageData(x,y,1,1).data[1],context.getImageData(x,y,1,1).data[2]] != [0,115,58]) {
+    var bluePixel = context.createImageData(1, 1);
+    bluePixel.data = [0, 113, 255, 255];
+    while ([context.getImageData(x, y, 1, 1).data[0], context.getImageData(x, y, 1, 1).data[1], context.getImageData(x, y, 1, 1).data[2]] != [0, 115, 58]) {
         context.drawImage(img, 0, 0, img.width, img.height);
         var x = array.random()[0];
         var y = array.random()[1];
-        dir = {horizontal:1, vertical:0};
-        while ([context.getImageData(x,y,1,1).data[0],context.getImageData(x,y,1,1).data[1],context.getImageData(x,y,1,1).data[2]] != [255,255,255]) {
+        dir = {
+            horizontal: 1,
+            vertical: 0
+        };
+        while ([context.getImageData(x, y, 1, 1).data[0], context.getImageData(x, y, 1, 1).data[1], context.getImageData(x, y, 1, 1).data[2]] != [255, 255, 255]) {
             x += 1;
         }
-        while ([context.getImageData(x,y,1,1).data[0],context.getImageData(x,y,1,1).data[1],context.getImageData(x,y,1,1).data[2]] == [255,255,255]) {
+        while ([context.getImageData(x, y, 1, 1).data[0], context.getImageData(x, y, 1, 1).data[1], context.getImageData(x, y, 1, 1).data[2]] == [255, 255, 255]) {
             move(1);
         }
-        if ([context.getImageData(x,y,1,1).data[0],context.getImageData(x,y,1,1).data[1],context.getImageData(x,y,1,1).data[2]] == [237,54,36]) {
+        if ([context.getImageData(x, y, 1, 1).data[0], context.getImageData(x, y, 1, 1).data[1], context.getImageData(x, y, 1, 1).data[2]] == [237, 54, 36]) {
             continue;
         }
         var flag = true;
@@ -48,28 +54,31 @@ function main() {
             var storedy = y;
             bounce();
             move(3);
-            if ([context.getImageData(x,y,1,1).data[0],context.getImageData(x,y,1,1).data[1],context.getImageData(x,y,1,1).data[2]] == [255,255,255]) {
+            if ([context.getImageData(x, y, 1, 1).data[0], context.getImageData(x, y, 1, 1).data[1], context.getImageData(x, y, 1, 1).data[2]] == [255, 255, 255]) {
                 break;
             }
             x = storedx;
             y = storedy;
             move(-3);
         } while (true);
-        context.putImageData(bluePixel,x,y);
+        context.putImageData(bluePixel, x, y);
     }
 }
+
 function move(num) {
     if (num >= 0) {
-        for (var i = 0; i<num; i++) {
+        for (var i = 0; i < num; i++) {
             x += dir.horizontal;
-            y += dir.vertical; 
+            y += dir.vertical;
         }
     } else {
-        for (var i = 0; i<num; i++) {
+        for (var i = 0; i < num; i++) {
             x -= dir.horizontal;
             y -= dir.vertical;
         }
+    }
 }
+
 function bounce() {
     if (dir.horizontal) {
         dir.horizontal = 0;
